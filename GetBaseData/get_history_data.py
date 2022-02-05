36a27dbecf60f49e7110b428cb223dfd002785b3
zhutaonan <adolf1321794021@gmail.com>
2022-02-05 17:22:35 +0800

    update

---
+++ GetBaseData/get_history_data.py
@@ -0,0 +1,35 @@
+# ！/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Project : stock_quant
+# @Date    : 2022/2/5 16:00
+# @Author  : Adolf
+# @File    : get_history_data.py
+# @Function:
+import requests
+import pandas as pd
+
+import akshare as ak
+
+pd.set_option("expand_frame_repr", False)
+
+# 历史行情数据
+stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol="000001",
+                                        period="daily",  # choice of {'daily', 'weekly', 'monthly'}
+                                        start_date="20170301",
+                                        end_date='20210907',
+                                        adjust="qfq")
+
+print(stock_zh_a_hist_df)
+
+# 分时数据
+stock_zh_a_hist_min_em_df = ak.stock_zh_a_hist_min_em(symbol="000001",
+                                                      # start_date="2021-09-01 09:32:00",
+                                                      # end_date="2021-09-06 09:32:00",
+                                                      period='5',  # choice of {'1', '5', '15', '30', '60'};
+                                                      adjust='qfq')
+# {'', 'qfq', 'hfq'}; '': 不复权, 'qfq': 前复权, 'hfq': 后复权,
+# 分时数据只能返回最近的,
+# 其中 1 分钟数据返回近 5 个交易日数据且不复权
+# TODO 寻找较长时间的分时数据源头
+
+print(stock_zh_a_hist_min_em_df)
---
+++ StrategyLib/ChanStrategy/automatic_drawing.py
@@ -0,0 +1,7 @@
+# ！/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Project : stock_quant
+# @Date    : 2022/2/5 17:22
+# @Author  : Adolf
+# @File    : automatic_drawing.py
+# @Function:
