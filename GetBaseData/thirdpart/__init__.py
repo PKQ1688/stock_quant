ca5afd20ada59ec207cd16b5930e979c81f3baf1
Adolf <adolf1321794021@gmail.com>
2022-02-08 18:03:32 +0800

    update

---
+++ GetBaseData/thirdpart/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-'''
+# @Project : stock_quant 
+# @Date    : 2022/2/8 16:08
+# @Author  : Adolf
+# @File    : __init__.py.py
--- StrategyLib/ChanStrategy/automatic_drawing.py
+++ StrategyLib/ChanStrategy/automatic_drawing.py
@@ -13,37 +13,49 @@ from StrategyLib.ChanStrategy.BasicChan.basic_tools import CZSC, RawBar
 from streamlit_echarts import st_pyecharts
 import streamlit.components.v1 as components
 
-symbol = st.sidebar.text_input(
-    'stock code', '000001')
+symbol = st.sidebar.text_input('stock code', '000001')
+period = st.sidebar.selectbox('time period', ('daily', 'weekly', 'monthly', '1min', '5min', '15min', '30min', '60min'))
+adjust = st.sidebar.selectbox('time period', ('qfq', 'hfq', 'origin'))
 
-period = st.sidebar.selectbox(
-    'time period', ('daily', 'weekly', 'monthly')
-)
-start_date = st.sidebar.text_input(
-    'start time', '20170301')
+start_date = None
+end_date = None
 
-end_date = st.sidebar.text_input(
-    'end time', '20210907')
+if period in ['daily', 'weekly', 'monthly']:
+    start_date = st.sidebar.text_input('start time', '20170301')
+    end_date = st.sidebar.text_input('end time', '20210907')
 
-adjust = st.sidebar.selectbox(
-    'time period', ('qfq', 'hfq', 'origin')
-)
 # 'You selected:', option
 
 st.title('股票数据展示')
 st.write("下面是表格")
 
-df = ak.stock_zh_a_hist(symbol=symbol,
-                        period=period,  # choice of {'daily', 'weekly', 'monthly'}
-                        start_date=start_date,
-                        end_date=end_date,
-                        adjust=adjust)
+if period in ['daily', 'weekly', 'monthly']:
+    df = ak.stock_zh_a_hist(symbol=symbol,
+                            period=period,  # choice of {'daily', 'weekly', 'monthly'}
+                            start_date=start_date,
+                            end_date=end_date,
+                            adjust=adjust)
+
+    bars = [RawBar(symbol=symbol, id=i, freq=period, open=row['开盘'], dt=row['日期'],
+                   close=row['收盘'], high=row['最高'], low=row['最低'], vol=row['成交量'],
+                   amount=row['成交额'])
+            for i, row in df.iterrows()]
+
+elif period in ['1min', '5min', '15min', '30min', '60min']:
+    df = ak.stock_zh_a_hist_min_em(symbol=symbol,
+                                   period=period.replace("min", ""),
+                                   adjust=adjust)
+
+    bars = [RawBar(symbol=symbol, id=i, freq=period, open=row['开盘'], dt=row['时间'],
+                   close=row['收盘'], high=row['最高'], low=row['最低'], vol=row['成交量'],
+                   amount=row['成交额'])
+            for i, row in df.iterrows()]
+
+else:
+    raise ValueError
 
 print(df)
-bars = [RawBar(symbol=symbol, id=i, freq=period, open=row['开盘'], dt=row['日期'],
-               close=row['收盘'], high=row['最高'], low=row['最低'], vol=row['成交量'],
-               amount=row['成交额'])
-        for i, row in df.iterrows()]
+
 
 ka = CZSC(bars)
 file_html = 'ShowHtml/czsc_render.html'
