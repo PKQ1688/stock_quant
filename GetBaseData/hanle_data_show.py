8f7db58f9242c7e9791f0c84742f9bb3990498d4
zhutaonan <adolf1321794021@gmail.com>
2022-07-02 22:51:49 +0800

    update

--- GetBaseData/hanle_data_show.py
+++ GetBaseData/hanle_data_show.py
@@ -54,6 +54,49 @@ def get_show_data(_df):
     }
 
 
+def show_data_from_df(df_or_dfpath):
+
+    if isinstance(df_or_dfpath, pd.DataFrame):
+        macd_df = df_or_dfpath[["MACD","SIGNAL","HISTOGRAM"]]
+        macd_df = macd_df.rename(
+            columns={"macd": "MACD_12_26_9", "histogram": "MACDh_12_26_9", "signal": "MACDs_12_26_9", })
+        # print(macd_df)
+
+    elif isinstance(df_or_dfpath, str):
+        df_or_dfpath = pd.read_csv(df_or_dfpath)
+
+        df_or_dfpath = df_or_dfpath[-300:]
+
+        macd_df = ta.macd(close=df_or_dfpath['close'])
+        macd_df.fillna(0, inplace=True)
+        # print(macd_df)
+        # print(_df)
+
+    else:
+        raise ValueError
+
+    datas = [list(oclh) for oclh in
+            zip(df_or_dfpath["open"].tolist(), df_or_dfpath["close"].tolist(), df_or_dfpath["high"].tolist(), df_or_dfpath["low"].tolist(),
+                df_or_dfpath["volume"].tolist(), macd_df["MACDh_12_26_9"].tolist(), macd_df["MACD_12_26_9"].tolist(),
+                macd_df['MACDs_12_26_9'].tolist())]
+
+    times = df_or_dfpath["date"].tolist()
+    vols = df_or_dfpath["volume"].tolist()
+    macds = macd_df["MACDh_12_26_9"].tolist()
+    difs = macd_df["MACD_12_26_9"].tolist()
+    deas = macd_df['MACDs_12_26_9'].tolist()
+
+
+    return {
+        "datas": datas,
+        "times": times,
+        "vols": vols,
+        "macds": macds,
+        "difs": difs,
+        "deas": deas,
+    }
+
+
 if __name__ == '__main__':
     csv_path = "Data/RealData/origin/600570.csv"
     # df = pd.read_csv(csv_path)
--- StrategyResearch/macd_deviate.py
+++ StrategyResearch/macd_deviate.py
@@ -2,24 +2,49 @@
 Description: 
 Author: adolf
 Date: 2022-07-02 14:18:51
-LastEditTime: 2022-07-02 16:13:39
+LastEditTime: 2022-07-02 21:58:10
 LastEditors: adolf
 '''
+from matplotlib import use
+import numpy as np
 import pandas as pd
+
 from finta import TA
-import pandas_ta as pdta
-from prometheus_client import Histogram
+from sympy import re
 from tqdm.auto import tqdm
+from scipy.signal import argrelextrema
+
+from Utils.ShowKline.base_kline import draw_chart
 
 pd.set_option("expand_frame_repr", False)
 pd.set_option("display.max_rows", None)
 
-df = pd.read_csv("Data/RealData/hfq/600570.csv")
-print(df[-10:])
+df = pd.read_csv("Data/RealData/hfq/002610.csv")
+# print(df[-200:])
 
 res_df = TA.MACD(df,period_fast=12,period_slow=26,signal=9,column="close")
 res_df["HISTOGRAM"] = res_df["MACD"] - res_df["SIGNAL"]
 # print(res_df[-10:])
 
-df = pd.concat(df,res_df)
-print(df[-10:])
+df = pd.concat([df,res_df],axis=1)
+use_df = df[-200:]
+use_df = use_df[['date','close','code','MACD','SIGNAL','HISTOGRAM']]
+# print(use_df)
+use_df['signal'] = 0
+# print(df[-10:])
+# 获取macd的极大值
+# res = argrelextrema(np.array(use_df['HISTOGRAM']),np.greater)[0].tolist()
+# 获取macd的极小值
+res = argrelextrema(np.array(use_df['HISTOGRAM']),np.less)[0].tolist()
+# print(res)
+use_df.iloc[res,'signal'] = 1
+# print(use_df.iloc[res,'is'])
+print(use_df)
+
+# plt.plot(np.array(use_df['HISTOGRAM']))
+# plt.scatter(
+#     argrelextrema(np.array(use_df['HISTOGRAM']), np.greater),
+#     np.array(use_df['HISTOGRAM'])[argrelextrema(np.array(use_df['HISTOGRAM']), np.greater)],
+#     c='red'
+# )
+# plt.show()
