ad4ef61252d1ef49200aa69214f2d5e2ad59fb0f
adolf <adolf1321794021@gmail.com>
2022-08-30 22:16:51 +0800

    update

--- MachineLearning/data_process/dask_data.py
+++ MachineLearning/data_process/dask_data.py
@@ -1,3 +1,10 @@
+'''
+Description: 
+Author: adolf
+Date: 2022-08-29 22:46:23
+LastEditTime: 2022-08-29 22:47:26
+LastEditors: adolf
+'''
 """
 Description: 
 Author: adolf
@@ -11,8 +18,9 @@ import dask.dataframe as dd
 
 def dask_one_data_process(data_path):
     data = dd.read_csv(data_path)
-    print(data.head())
     # data = data.drop(columns=["code", "pct"])
+    data = data[["open", "high", "low", "close", "volume", "turn", "pctChg"]]
+    print(data.head())
     # data.to_csv(data_path, index=False)
 
-dask_one_data_process("Data/HandleData/hfq_stock/handle_000001.csv")
+dask_one_data_process("Data/RealData/hfq/000001.csv")
