883301d082c95cacf3b102e4f8be258824ecd6d8
zhutaonan <adolf1321794021@gmail.com>
2022-12-18 13:26:24 +0800

    update

--- StrategyResearch/data_research/compare_sk_ta.py
+++ StrategyResearch/data_research/compare_sk_ta.py
@@ -21,27 +21,32 @@ def cal_one_board_mom(board_data_path, period=20):
     data = data[["date", "open", "high", "low", "close", "volume"]]
     data = data[-period:]
     data.reset_index(drop=True, inplace=True)
-    data["mid"] = (data["open"] + data["close"] + data["high"] + data["low"]) / 4
+    # data["mid"] = (data["open"] + data["close"] + data["high"] + data["low"]) / 4
 
     model = LinearRegression()
     x = np.linspace(0, 1, period).reshape(-1, 1)
 
     y_close = data.close.values.reshape(-1, 1)
-    y_close = normalization(y_close)
+    # y_close = normalization(y_close)
     model.fit(x, y_close)
 
     # print(model.coef_[0][0])
     R2 = model.score(x, y_close)
-    return model.coef_[0][0], R2
+    return model.coef_, R2
+
 
 def use_ta_cal_one_board_mom(board_data_path, period=20):
     data = pd.read_csv("Data/BoardData/industry_origin/" + board_data_path)
     data = data[["date", "open", "high", "low", "close", "volume"]]
     data = data[-period:]
     data.reset_index(drop=True, inplace=True)
-    data["mid"] = (data["open"] + data["close"] + data["high"] + data["low"]) / 4
-    data["mom"] = ta.linreg(data.close, length=period)
-    return data.mom.values[-1]
+    # data["mid"] = (data["open"] + data["close"] + data["high"] + data["low"]) / 4
+    res = ta.linreg(data.close,
+                    length=period,
+                    r=True)
+    return res
+
 
 if __name__ == '__main__':
     print(cal_one_board_mom("汽车整车.csv"))
+    print(use_ta_cal_one_board_mom("汽车整车.csv"))
