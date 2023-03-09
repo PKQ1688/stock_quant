ee754c6504f6a35010c0942a87cb1f4cc284b3de
adolf <adolf1321794021@gmail.com>
2023-03-09 22:38:49 +0800

    update

--- GetBaseData/hanle_data_show.py
+++ GetBaseData/hanle_data_show.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-'''
-# @Project : stock_quant 
+# @Project : stock_quant
 # @Date    : 2022/1/6 17:09
 # @Author  : Adolf
 # @File    : hanle_data_show.py
 import pandas as pd
 from finta import TA
 from loguru import logger
+from datetime import datetime
+
 # import pandas_ta as ta
 
 
@@ -56,11 +58,13 @@ from loguru import logger
 #     }
 
 
-def show_data_from_df(df_or_dfpath: str=None,use_all_data:bool=False):
+def show_data_from_df(
+    df_or_dfpath: str = None, use_all_data: bool = True, end_date: str = None
+):
 
     if isinstance(df_or_dfpath, pd.DataFrame):
         try:
-            macd_df = df_or_dfpath[["MACD","SIGNAL","HISTOGRAM"]]
+            macd_df = df_or_dfpath[["MACD", "SIGNAL", "HISTOGRAM"]]
         # print(macd_df)
         except:
             macd_df = TA.MACD(df_or_dfpath)
@@ -68,7 +72,6 @@ def show_data_from_df(df_or_dfpath: str=None,use_all_data:bool=False):
 
     elif isinstance(df_or_dfpath, str):
         df_or_dfpath = pd.read_csv(df_or_dfpath)
-
 
         macd_df = TA.MACD(df_or_dfpath)
         macd_df["HISTOGRAM"] = macd_df["MACD"] - macd_df["SIGNAL"]
@@ -79,34 +82,51 @@ def show_data_from_df(df_or_dfpath: str=None,use_all_data:bool=False):
 
     else:
         raise ValueError("df_or_dfpath must be str or pd.DataFrame")
-    
+
+    # df_or_dfpath = df_or_dfpath[-60:]
+    # macd_df = macd_df[-60:]
+
+    df_or_dfpath = pd.concat([df_or_dfpath, macd_df], axis=1)
+    # import pdb
+    # pdb.set_trace()
+    # if start_date is not None:
+        # df_or_dfpath = df_or_dfpath[df_or_dfpath["date"] >= start_date]
+        # macd_df = macd_df[macd_df["date"] >= start_date]
+    if end_date is not None:
+        df_or_dfpath = df_or_dfpath[df_or_dfpath["date"] <= end_date]
+        # macd_df = macd_df[macd_df["date"] <= end_date]
+
     df_or_dfpath = df_or_dfpath[-60:]
-    macd_df = macd_df[-60:]
-
-
-    datas = [list(oclh) for oclh in
-            zip(df_or_dfpath["open"].tolist(), df_or_dfpath["close"].tolist(), df_or_dfpath["high"].tolist(), df_or_dfpath["low"].tolist())]
+    datas = [
+        list(oclh)
+        for oclh in zip(
+            df_or_dfpath["open"].tolist(),
+            df_or_dfpath["close"].tolist(),
+            df_or_dfpath["high"].tolist(),
+            df_or_dfpath["low"].tolist(),
+        )
+    ]
     if "index" in df_or_dfpath:
-        df_or_dfpath['date'] =df_or_dfpath['date']+ '_' +  df_or_dfpath['index'].map(str)
+        df_or_dfpath["date"] = (
+            df_or_dfpath["date"] + "_" + df_or_dfpath["index"].map(str)
+        )
 
-    
     # buy_list = df_or_dfpath["buy"].tolist() if "buy" in df_or_dfpath else []
     # sell_list = df_or_dfpath["sell"].tolist() if "sell" in df_or_dfpath else []
-
 
     return {
         "datas": datas,
         "times": df_or_dfpath["date"].tolist(),
         "vols": df_or_dfpath["volume"].tolist(),
-        "macds": macd_df["HISTOGRAM"].tolist(),
-        "difs": macd_df["MACD"].tolist(),
-        "deas": macd_df['SIGNAL'].tolist(),
+        "macds": df_or_dfpath["HISTOGRAM"].tolist(),
+        "difs": df_or_dfpath["MACD"].tolist(),
+        "deas": df_or_dfpath["SIGNAL"].tolist(),
         # "buy": buy_list,
         # "sell": sell_list
     }
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     csv_path = "Data/RealData/qfq/600570.csv"
     print(show_data_from_df(df_or_dfpath=csv_path))
     # draw_chart(show_data_from_df(df_or_dfpath=csv_path))
--- MachineLearning/annotation_platform/buy_and_sell_signals.py
+++ MachineLearning/annotation_platform/buy_and_sell_signals.py
@@ -1,13 +1,17 @@
 """
-Author       : adolf adolf1321794021@gmail.com
-Date         : 2023-03-05 19:02:48
-LastEditors  : adolf
-LastEditTime : 2023-03-05 22:39:07
-FilePath     : /stock_quant/MachineLearning/annotation_platform/buy_and_sell_signals.py
-Description  : 
+@Author       : adolf adolf1321794021@gmail.com
+@Date         : 2023-03-09 21:40:11
+@LastEditors  : adolf
+@LastEditTime : 2023-03-09 22:37:48
+@FilePath     : /stock_quant/MachineLearning/annotation_platform/buy_and_sell_signals.py
+@Description  : 
 """
+import os
+from datetime import datetime, timedelta
+import random
 import numpy as np
 import pandas as pd
+import akshare as ak
 import streamlit as st
 
 from streamlit_echarts import st_pyecharts
@@ -17,28 +21,61 @@ from Utils.ShowKline.base_kline import draw_chart
 
 st.set_page_config(page_title="股票买卖标注平台", layout="wide")
 
+# 缓存内存
+if "code_name" not in st.session_state:
+    st.session_state["code_name"] = "600570"
+
+if "end_time" not in st.session_state:
+    st.session_state["end_time"] = None
+
+
+def base_show_fun(code_name, end_time):
+    # start_time = datetime.strftime(start_time, '%Y-%m-%d')
+    end_time = datetime.strftime(end_time, "%Y-%m-%d")
+
+    show_data = show_data_from_df(
+        f"Data/RealData/hfq/{code_name}.csv", end_date=end_time
+    )
+    chart = draw_chart(show_data)
+    st_pyecharts(chart, height="600%", width="100%")
+
+    trade_result = st.radio(label="操作", options=["买", "卖", "保持"])
+    next_button = st.button("next day")
+
+    return trade_result, next_button, show_data
+
+
 st.title("买卖信号标注平台")
+
 
 label_tab, dataset_tab = st.tabs(["Label", "Dataset"])
 
+code_list = os.listdir("Data/RealData/hfq")
+
 with label_tab:
-    show_data = show_data_from_df("Data/RealData/hfq/600570.csv")
-    chart = draw_chart(show_data)
-    st_pyecharts(chart, height="200%", width="100%")
+    randpm_code = st.button("随机股票")
+    if randpm_code:
+        st.session_state["code_name"] = random.choice(code_list).split(".")[0]
+    st.write(f'股票代码:{st.session_state["code_name"]}')
 
-    trade_result = st.radio(label="操作", options=["买", "卖", "保持"])
+    # start_time = st.date_input("开始时间", value=pd.to_datetime("2019-01-01"))
+    if st.session_state["end_time"] is None:
+        st.session_state["end_time"] = st.date_input("结束时间", value=pd.to_datetime("2020-01-01"))
+    
 
-    next_button = st.button("next day")
+    trade_result, next_button, show_data = base_show_fun(st.session_state["code_name"], st.session_state["end_time"])
+
     if next_button:
         with open("Data/LabelData/total_dataset.tsv", "a") as f:
             save_data = []
-            save_data.append("600570")
+            save_data.append(st.session_state["code_name"])
             save_data.append(show_data["times"][0])
             save_data.append(show_data["times"][-1])
             save_data.append(trade_result)
             line = "\t".join(save_data)
             f.write(f"{line}\n")
 
+        st.session_state["end_time"] = st.session_state["end_time"] + timedelta(days=1)
         st.success("保存成功", icon="✅")
 
 with dataset_tab:
@@ -51,4 +88,4 @@ with dataset_tab:
         np.array(rank_texts_list),
         columns=(["code", "start_time", "end_time", "trade_result"]),
     )
-    st.dataframe(df,use_container_width=True)
+    st.dataframe(df, use_container_width=True)
