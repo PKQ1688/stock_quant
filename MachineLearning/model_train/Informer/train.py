256b67932756654652ce974e8965f7d0bf91b129
adolf <adolf1321794021@gmail.com>
2023-04-05 21:54:08 +0800

    update

---
+++ MachineLearning/model_train/Informer/train.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+"""
+@Project ：stock_quant 
+@File    ：train.py
+@Author  ：adolf
+@Date    ：2023/4/5 21:41 
+"""
+from datasets import load_dataset
+
+dataset = load_dataset("monash_tsf", "traffic_hourly")
+print(dataset)
