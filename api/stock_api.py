597281f899c8cd820216d85705a2687e1dc5e741
adolf <adolf1321794021@gmail.com>
2023-04-20 10:42:04 +0800

    tmp

--- api/stock_api.py
+++ api/stock_api.py
@@ -1,7 +1,7 @@
 import time
 from pyecharts.components import Table
 import baostock as bs
-from fastapi import FastAPI,Body,Response
+from fastapi import FastAPI, Body, Response
 import uvicorn
 from pymongo import MongoClient
 
@@ -12,33 +12,41 @@ db = MongoClient(mongo_config["host"], mongo_config["port"])["stock_db"]
 
 
 @app.post("/stock_data")
-def get_stock_data(code = Body(None) , start_date= Body(None), end_date= Body(None),frequency= Body(None)):
+def get_stock_data(
+    code=Body(None), start_date=Body(None), end_date=Body(None), frequency=Body(None)
+):
     if start_date is None:
         start_date = "2022-12-19"
     if end_date is None:
         end_date = "2023-04-10"
     if frequency is None:
         frequency = "d"
-    print(f"code:{code} start_date:{start_date} end_date:{end_date} frequency:{frequency}")
+    print(
+        f"code:{code} start_date:{start_date} end_date:{end_date} frequency:{frequency}"
+    )
     rs = bs.query_history_k_data_plus(
         code,
         "date,open,close,high,low,volume,amount",
-        start_date=start_date, end_date=end_date,
-        frequency=frequency, adjustflag="1")
+        start_date=start_date,
+        end_date=end_date,
+        frequency=frequency,
+        adjustflag="1",
+    )
     # 打印结果集
     data_list = []
-    while (rs.error_code == '0') & rs.next():
+    while (rs.error_code == "0") & rs.next():
         # 获取一条记录，将记录合并在一起
         data = rs.get_row_data()
-        for i in range(1,6):
+        for i in range(1, 6):
             data[i] = round(float(data[i]), 3)
         data_list.append(data)
     # result = pd.DataFrame(data_list, columns=rs.fields)
     print(f"result:{data_list}")
-    #将data_list的第1列的全部数据保留3位小数
+    # 将data_list的第1列的全部数据保留3位小数
     return data_list
 
 
+<<<<<<< Updated upstream
 
 @app.post("/get_records")
 def get_records(user_id = Body(None),start_date= Body(None), end_date= Body(None),stock_code = Body(None)):
@@ -53,6 +61,26 @@ def get_records(user_id = Body(None),start_date= Body(None), end_date= Body(None
             filter["date"]["$lte"] = end_date
         else:
             filter["date"]= {"$lte":end_date}
+=======
+@app.post("/get_records")
+def get_records(
+    user_id=Body(None),
+    start_date=Body(None),
+    end_date=Body(None),
+    stock_code=Body(None),
+):
+    print("user_id:", user_id)
+    filter = {}
+    if user_id is not None and user_id != "":
+        filter["user_id"] = user_id
+    if start_date is not None and start_date != "":
+        filter["date"] = {"$gte": start_date}
+    if end_date is not None and end_date != "":
+        if "date" in filter:
+            filter["date"]["$lte"] = end_date
+        else:
+            filter["date"] = {"$lte": end_date}
+>>>>>>> Stashed changes
     if stock_code is not None:
         filter["stock_code"] = stock_code
     print(f"filter:{filter}")
@@ -60,6 +88,7 @@ def get_records(user_id = Body(None),start_date= Body(None), end_date= Body(None
     for history in db["play_records"].find(filter):
         history.pop("_id")
         history.pop("records")
+<<<<<<< Updated upstream
         print("history: ",history)
         return_records.append([history["user_id"],history["date"],history["stock_code"],history["stock_profit_rate"],history["profit_rate"]])
     return return_records
@@ -74,25 +103,77 @@ def push_records(records = Body(None),user_id = Body(None),stock_code = Body(Non
     today = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
     db["play_records"].insert_one({"user_id":user_id,"records":records,"profit_rate":profit_rate,"date":today,"stock_code":stock_code,"stock_profit_rate":stock_profit_rate})
     return f"success,profit_rate is {profit_rate}"
+=======
+        print("history: ", history)
+        return_records.append(
+            [
+                history["user_id"],
+                history["date"],
+                history["stock_code"],
+                history["stock_profit_rate"],
+                history["profit_rate"],
+            ]
+        )
+    return return_records
+
+
+@app.post("/push_records")
+def push_records(
+    records=Body(None),
+    user_id=Body(None),
+    stock_code=Body(None),
+    stock_profit_rate=Body(None),
+):
+    print(f"request body : {Body}")
+    print(f"get {user_id} records from browser:{records}")
+    profit_rate = cal_profit_rate(records)
+    today = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+    db["play_records"].insert_one(
+        {
+            "user_id": user_id,
+            "records": records,
+            "profit_rate": profit_rate,
+            "date": today,
+            "stock_code": stock_code,
+            "stock_profit_rate": stock_profit_rate,
+        }
+    )
+    return f"success,profit_rate is {profit_rate}"
+
+>>>>>>> Stashed changes
 
 def cal_profit_rate(records):
     profit_rate = 1
     assert len(records) % 2 == 0
     i = 0
     while i < len(records):
+<<<<<<< Updated upstream
         profit_rate = records[i+1]["price"] / records[i]["price"] * profit_rate
         i = i + 2
     return str(round((profit_rate-1)*100,2)) +'%'
+=======
+        profit_rate = records[i + 1]["price"] / records[i]["price"] * profit_rate
+        i = i + 2
+    return str(round((profit_rate - 1) * 100, 2)) + "%"
+>>>>>>> Stashed changes
 
-@app.get('/index')
+
+@app.get("/index")
 def func():
-    with open('api/test.html', 'r', encoding='utf8') as file:
+    with open("api/test.html", "r", encoding="utf8") as file:
         content = file.read()
     # 4.返回响应数据
-    return Response(content=content, media_type='text/html')
+    return Response(content=content, media_type="text/html")
 
 
+@app.get("/hist")
+def func():
+    with open("api/hist.html", "r", encoding="utf8") as file:
+        content = file.read()
+    # 4.返回响应数据
+    return Response(content=content, media_type="text/html")
 
+<<<<<<< Updated upstream
 @app.get('/hist')
 def func():
     with open('api/hist.html', 'r', encoding='utf8') as file:
@@ -103,3 +184,8 @@ def func():
 
 if __name__ == '__main__':
     uvicorn.run(app, host='172.22.67.15', port=9999)
+=======
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="172.22.67.15", port=9999)
+>>>>>>> Stashed changes
