eb7cd073bff4e395940ed5d892880469306f9197
weixiaotong <weixiaotong@hyperchain.cn>
2023-04-21 15:59:50 +0800

    modify tests

--- api/test.py
+++ api/test.py
@@ -1,14 +1,14 @@
 import requests
 
 def test_push_records():
-    url = 'http://172.22.67.15:9999/push_records'
+    url = 'http://localhost:9999/push_records'
     json_param = [{"opt":"buy","date":"2021-09-09","price":1200},{"opt":"buy","date":"2021-09-09","price":2000}]
     rec = {"user_id":"rose","records":json_param,"stock_code":"000111","stock_profit_rate":"1"}
     response = requests.post(url, json=rec)
     print(f"response {response.text}")
 
 def test_get_code_data():
-    url = "http://172.22.67.15:9999/stock_data"
+    url = "http://localhost:9999/stock_data"
     rec = {
         "start_date": "2023-04-19",
         "end_date": "2023-04-19",
