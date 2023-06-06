da5f81cf1ea7250ab74534df5d8cb384bab6d987
weixiaotong <weixiaotong@hyperchain.cn>
2023-06-06 22:55:11 +0800

    modify stable dog

--- StrategyLib/AutomaticInvestmentPlan/result_show.py
+++ StrategyLib/AutomaticInvestmentPlan/result_show.py
@@ -7,9 +7,7 @@
 """
 from StrategyLib.AutomaticInvestmentPlan.stable_dog import get_AI_plan_result
 import streamlit as st
-import matplotlib.pyplot as plt
 from datetime import datetime
-
 
 def getDays(day1, day2):
     # 获取需要计算的时间戳
@@ -20,14 +18,14 @@ def getDays(day1, day2):
 
 
 def auto_investment_plan():
-    st.subheader("Stable ")
+    st.subheader("定投回测结果")
     code = st.sidebar.text_input("code", value="sz.399006")
     gap_days = st.sidebar.text_input("interval", value=1)
     first_buy_day = st.sidebar.text_input("start", value="2019-01-02")
-    want_rate = st.sidebar.text_input("target", value=1.5)
+    want_rate = st.sidebar.text_input("target", value=1.1)
     if_intelli = st.sidebar.text_input("if_intelli", value="yes")
     threshold = st.sidebar.text_input("threshold", value=500000)
-
+    st.text("定投记录表：")
     res, stock_data = get_AI_plan_result(code=code,
                                          gap_days=int(gap_days),
                                          first_buy_day=first_buy_day,
@@ -40,6 +38,9 @@ def auto_investment_plan():
     st.text(
         f"达成目标自然日天数:{natual_day}，投入次数/总金额:{len(res[res['put'] != 0])}/{int(res.loc[len(res) - 1, 'put_in'])}，总收益:{int(res.loc[len(res) - 1, 'account'] - res.loc[len(res) - 1, 'put_in'])}，收益率:{round(res.loc[len(res) - 1, 'rate'], 3)},标的涨跌幅:{stock_data.loc[len(stock_data) - 1, 'close'] / stock_data.loc[0, 'open']}")
     chart_data = res[["rate"]].apply(lambda x: (x - 1) * 100)
+    st.text("定投收益率：")
     st.line_chart(chart_data, y="rate")
+    st.text("达成目标收益率时投入总金额：")
     st.line_chart(res, y="put_in")
+    st.text("股票行情：")
     st.line_chart(stock_data, y="close", x='date')
